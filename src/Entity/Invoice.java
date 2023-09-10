27e6ab97911a07a69b78c25eaeea75bde7df128e
DELL <DELL@DESKTOP-R4Q2FSQ>
2023-09-10 10:38:08 +0530

    Invoice entity created

---
+++ src/Entity/Invoice.java
@@ -0,0 +1,29 @@
+package Entity;
+
+public class Invoice {
+	
+	private int invoiceNo;
+	private int poNo;
+	private String gstNo;
+	public int getInvoiceNo() {
+		return invoiceNo;
+	}
+	public void setInvoiceNo(int invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+	public int getPoNo() {
+		return poNo;
+	}
+	public void setPoNo(int poNo) {
+		this.poNo = poNo;
+	}
+	public String getGstNo() {
+		return gstNo;
+	}
+	public void setGstNo(String gstNo) {
+		this.gstNo = gstNo;
+	}
+	
+	
+
+}
