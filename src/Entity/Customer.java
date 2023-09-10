30c5f8582d9f04275889fc2e56f86a58887261b9
91952 <91952@DINESH45>
2023-09-10 11:02:25 +0530

    Customer Entity created

---
+++ src/Entity/Customer.java
@@ -0,0 +1,28 @@
+package Entity;
+
+public class Customer {
+
+	private int cId;
+	private String cName;
+	private long cContact;
+	public int getcId() {
+		return cId;
+	}
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public long getcContact() {
+		return cContact;
+	}
+	public void setcContact(long cContact) {
+		this.cContact = cContact;
+	}
+	
+	
+}
--- src/Nursory/MainApp.java
+++ src/Nursory/MainApp.java
@@ -1,8 +1,6 @@
 package Nursory;
-
 public class MainApp {
   public static void main(String[] args) {
 	System.out.println("hello word");
    }
-	
 }
